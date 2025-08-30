2f41f7884f0f199a317aa731a06d643f4ae90835
smrt <samratsonawane047@gmail.com>
2025-08-30 21:53:35 +0530

    Updated with changes today

--- app.py
+++ app.py
@@ -1,25 +1,52 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app= Flask(__name__)
 
-tasks=[]
+#connect to database
+def get_db_connection():
+    conn=sqlite3.connect("todo.db")
+    conn.row_factory=sqlite3.Row
+    return conn
+
+#initialize db
+def init_db():
+    conn=get_db_connection()
+    conn.execute('''
+                create table if not exists tasks(
+                id integer primary key autoincrement,
+                task text not null)''')
+    conn.commit()
+    conn.close()
+
+init_db()   #run once at startup
 
 @app.route("/")
 def index():
-    return render_template("index.html",tasks=tasks)
+        conn=get_db_connection()
+        tasks=conn.execute("select * from tasks").fetchall()
+        conn.close()
+        return render_template("index.html",tasks=tasks)
 
 @app.route("/add",methods=["POST"])
 def add_task():
     task=request.form.get("task")
     if task:
-        tasks.append(task)
+        conn=get_db_connection()
+        conn.execute("insert into tasks(task) values(?)",(task,))
+        conn.commit()
+        conn.close()
     return redirect(url_for("index"))
+
 
 @app.route("/delete/<int:task_id>")
 def delete_task(task_id):
-    if 0<=task_id<len(tasks):
-        tasks.pop(task_id)
+    conn=get_db_connection()
+    conn.execute("delete from tasks where id=?",(task_id,))
+    conn.commit()
+    conn.close()
     return redirect(url_for("index"))
+
 
 if __name__=="__main__":
     app.run(debug=True)
